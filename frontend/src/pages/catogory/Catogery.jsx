afffede4f8fb36b7cbca8bce06c52d6db9dfa2a7
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-02 14:16:40 +0530

    s

---
+++ frontend/src/pages/catogory/Catogery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+function Catogery() {
+  return (
+    <>
+    <Navbar/>   
+    </>
+  )
+}
+
+export default Catogery
