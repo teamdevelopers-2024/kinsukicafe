6fcfcdfbe71ea7b1bb2ca9f4c2f1f8641ab909f5
teamdevelopers-2024 <teamdevelopers450@gmail.com>
2024-10-05 01:16:51 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "https://kinsukicafe.vercel.app/api", 
+  baseURL: "https://kinsukicafe-api.vercel.app/api", 
   headers: {
     "Content-Type": "application/json",
   },
