91d04f1d7cbec70f062ad0faa03229a8d6eac206
MUHAMMED SINAN MP <143805783+sinanmp@users.noreply.github.com>
2024-10-04 15:50:56 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,7 +10,7 @@ import axios from "axios";
 // })
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api", 
+  baseURL: "https://kinsukicafe-api.vercel.app/api", 
   headers: {
     "Content-Type": "application/json",
   },
