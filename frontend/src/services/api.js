148e58c6ba41d1943a4f390f735b4866cdf7c4fc
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-10-04 21:19:18 +0530

    hello

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 
 
-// const api = axios.create({
-//   baseURL: "http://localhost:3001/api", 
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// })
-
 const api = axios.create({
   baseURL: "http://localhost:3001/api", 
   headers: {
@@ -16,6 +8,14 @@ const api = axios.create({
   },
   withCredentials: true,
 })
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3001/api", 
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true,
+// })
 
 
 
