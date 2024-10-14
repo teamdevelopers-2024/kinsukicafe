c75398a8ef607964bffc382219d926fc74e99928
Mpdanish <danishmaliyekkal786@gmail.com>
2024-10-14 16:43:43 +0530

    pdf button fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 
-// const api = axios.create({
-//   baseURL: "https://kinsukicafe-api.vercel.app/api", 
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// })
-
 const api = axios.create({
-  baseURL: "http://localhost:3001/api", 
+  baseURL: "https://kinsukicafe-api.vercel.app/api", 
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 })
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3001/api", 
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true,
+// })
 
 
 
